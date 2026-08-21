67dc7ccef300e08ad44f8c07feb229b4e14b56f7
rui-ktei <rui.cheng@sympli.com.au>
2026-08-22 02:55:05 +1000

    Stop spans running past the last timestamp

--- windowing.py
+++ windowing.py
@@ -4,5 +4,7 @@ def bucket_of(timestamp, width):
 
 def spans(timestamps, width):
     ordered = sorted(timestamps)
-    return [bucket_of(ordered[i + 1], width) - bucket_of(ordered[i], width)
-            for i in range(len(ordered))]
+    return [
+        bucket_of(ordered[index + 1], width) - bucket_of(ordered[index], width)
+        for index in range(len(ordered) - 1)
+    ]
