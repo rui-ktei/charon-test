03c6b40c67b37f93696ca9ed49b629db7ef46472
rui.cheng <rui.cheng@sympli.com.au>
2026-08-22 14:21:34 +1000

    Wrap the shutdown window around midnight

--- windows.py
+++ windows.py
@@ -1,1 +1,8 @@
-WINDOW_LIMIT = 25
+SHUTDOWN_HOUR = 19
+STARTUP_HOUR = 7
+
+
+def within_window(hour: int) -> bool:
+    if STARTUP_HOUR <= SHUTDOWN_HOUR:
+        return STARTUP_HOUR <= hour < SHUTDOWN_HOUR
+    return hour >= STARTUP_HOUR or hour < SHUTDOWN_HOUR
