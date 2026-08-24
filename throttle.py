a7630f5f9aabf7d9ace68764e360d92deb81993f
rui-ktei <rui.cheng@sympli.com.au>
2026-08-24 23:55:55 +1000

    Add a throttle helper

---
+++ throttle.py
@@ -0,0 +1,14 @@
+THROTTLE_LIMIT = 30
+WINDOW_SECONDS = 60
+
+
+def rate_of(calls, window_seconds=WINDOW_SECONDS):
+    return calls / window_seconds
+
+
+def allowed(calls, window_seconds=WINDOW_SECONDS):
+    return rate_of(calls, window_seconds) <= THROTTLE_LIMIT / WINDOW_SECONDS
+
+
+def remaining(calls):
+    return THROTTLE_LIMIT - calls
